23e67e94f7efa4cff85ecd86db4eeb20b5955ee2
Yashwanth B M <yashwanthbm36@gmail.com>
2023-11-29 23:55:04 +0530

    learning RTK

---
+++ 07_reduxToolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,33 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos:[{id:1,text:" First Todo!.. " }]
+}
+
+export const todoSlice = createSlice({
+    name:"Todo",
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const todo ={
+                id:nanoid(), 
+                text:action.payload
+            }
+            state.todos.push(todo);
+        },
+        removeTodo : (state,action)=>{
+            state.todos = state.todos.filter(todo=>(todo.id !== action.payload))
+        },
+/*         updateTodo:(state,action)=>{
+            state.todos = state.todos.map(todo=>{
+                if(todo.id === action.payload.id){
+                    todo = {...todo,text:action.payload.text}
+                }
+                return todo;
+            })
+        }, */
+    }
+})
+
+export const {addTodo,removeTodo} = todoSlice.actions;
+export default todoSlice.reducer;
